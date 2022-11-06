2026d7310c44ae52851a11aadcd79e26854a7913
callanbrombacher <callan@sitebydesgin.co.za>
2022-11-06 20:39:45 +0200

    fixed github errors, due to 2fa issues

--- pages/_app.tsx
+++
@@ -1,9 +0,0 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
-import { wrapper } from "../src/redux/store/store";
-
-function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
-}
-
-export default wrapper.withRedux(App);
