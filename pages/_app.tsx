63bcded142b7c16c10bb24d00c1b2db1d8d898b9
callanbrombacher <callan@sitebydesgin.co.za>
2022-11-07 03:43:03 +0200

    webpage content responsiveness added for sticky header

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -5,8 +5,25 @@ import {UserCtx, defaultUserCtx} from "../src/context/UserContext.component";
 import {Provider} from 'react-redux'
 import Header from '../src/components/templates/header.component'
 import Footer from '../src/components/templates/footer.component'
+import {useEffect, useRef, useCallback, } from "react";
 function App({ Component, ...rest }: AppProps) {
+
   const {store, props} = wrapper.useWrappedStore(rest);
+    const contentRef =  useRef<HTMLDivElement>(null)
+
+    const listenToScroll = ():void => {
+        const content = contentRef.current
+        if(content !==null){
+        const marginTop = parseInt((window.getComputedStyle(content)
+            .getPropertyValue("margin-top"))
+            .replace('px', ''))
+        if( window.scrollY ===0) content.style.marginTop = ''
+        if( window.scrollY !==0 && content.style.marginTop==='')content.style.marginTop = (marginTop - 50) + "px"
+    }}
+
+    useEffect(()=>{
+        window.addEventListener('scroll', listenToScroll)
+    }, [])
 
   return (
       <Provider store={store}>
@@ -14,9 +31,9 @@ function App({ Component, ...rest }: AppProps) {
               value={defaultUserCtx}
           >
              <Header/>
-              <section className={'webpage-content'}>
+              <div ref={contentRef} className={'webpage-content'}>
              <Component {...props.pageProps} />
-              </section>
+              </div>
             <Footer/>
           </UserCtx.Provider>
       </Provider>
